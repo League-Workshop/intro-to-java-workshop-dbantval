5011d2af4a8f7f5c55526ac36c786bc57af20222
League Student <league@iMac.attlocal.net>
2019-07-09 14:30:44 -0700

    Day 2
    ........................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -6,18 +6,26 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
-
+	Robot mORETeriBeLStUfF  = new Robot();
+	
 	void go() {
 		// 4. Make the robot move as fast as possible
+		mORETeriBeLStUfF.setSpeed(100);
+		mORETeriBeLStUfF.penDown();
+		mORETeriBeLStUfF.sparkle();
 
 		// 5. Set the pen width to 5
+		mORETeriBeLStUfF.setPenWidth(5);
 
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
+		
 
 			// 7. Set the pen color to random
+		mORETeriBeLStUfF.setRandomPenColor();
+		
 	
 			// 1. Call the drawSquare() method
-	
+	drawSquare();
 			// 8. Turn the robot 90 degrees to the right
 
 	}
@@ -26,6 +34,10 @@ public class FourSquare {
 	void drawSquare() {
 		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 		/* 3. Fill in the code to draw a square inside the method below. */
+		for(int i = 0; i<4; i++) {
+			mORETeriBeLStUfF.move(150);
+			mORETeriBeLStUfF.turn(90);
+		}
 		
 		
 	}
--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,19 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
-		
-		
+	Robot tIEreIbleSTUFF = new Robot();
+	tIEreIbleSTUFF.penDown();
+	tIEreIbleSTUFF.setSpeed(99);	
+	tIEreIbleSTUFF.move(150);
+	tIEreIbleSTUFF.turn(90);
+	tIEreIbleSTUFF.move(150);
+	tIEreIbleSTUFF.turn(90);
+	tIEreIbleSTUFF.move(150);
+	tIEreIbleSTUFF.turn(90);
+	tIEreIbleSTUFF.move(150);
+	for(int i = 0; i<4; i++) {
+		tIEreIbleSTUFF.move(150);
+		tIEreIbleSTUFF.turn(90);
+	}
 	}
 }
