8f8ccbb97faefc0d4a5ccb15e1e89394c1ba3f21
League Student <league@iMac-2.local>
2019-07-10 14:23:55 -0700

    DAY 3
    trash is trash

---
+++ src/section3/HelloWorld.java
@@ -0,0 +1,12 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class HelloWorld {
+public static void main(String[] args) {
+System.out.println("hello bad stuff");
+JOptionPane.showMessageDialog(null, "hello bad stuff");
+}
+
+}
+
--- src/section3/WorldDomination.java
+++ src/section3/WorldDomination.java
@@ -6,11 +6,17 @@ public class WorldDomination {
 	
 	public static void main(String[] args) {
 		// 1. Ask the user if they know how to write code.
+	String answer =	JOptionPane.showInputDialog("Do you know how to code?");
 
 		// 2. If they say "yes", tell them they will rule the world.
-
-		// 3. Otherwise, wish them good luck washing dishes.
-
+	if(answer.equalsIgnoreCase("Yes")) {
+	JOptionPane.showMessageDialog(null, "You will rule the world");
 	}
+		// 3. Otherwise, wish them good luck washing dishes.
+	
+if(answer.equalsIgnoreCase("No")) {
+	JOptionPane.showMessageDialog(null, "Enjoy washing dishes");
+}
+}
 }
 
